a6b530f22ee44950736171146bdf9c29309e5284
campellocruz <guilhermecampellolacannacruz@gmail.com>
2026-06-09 20:19:47 -0300

    Concluído -> Exercício 2: Cadastro de Produto

---
+++ Exercício 2 - Cadastro de Produto/Program.cs
@@ -0,0 +1,7 @@
+﻿var nomedoproduto = "Mouse Logitech";
+var preco = 259.90m;
+var quantidade = 23;
+
+Console.WriteLine($"Produto: {nomedoproduto}");
+Console.WriteLine($"Preço: R$ {preco}");
+Console.WriteLine($"Quantidade em Estoque: {quantidade}");
