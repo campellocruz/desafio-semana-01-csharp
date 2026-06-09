363c3ce240442ff950925999b9113786e07b7c38
campellocruz <guilhermecampellolacannacruz@gmail.com>
2026-06-09 19:42:40 -0300

    Concluído -> Exercício 1: Aprensentação Pessoal

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿var name = "Guilherme Campello";
+var age = 17;
+var city = "Nova Iguaçu";
+var dreamprofission = "Cantor/Produtor Musical";
+
+Console.WriteLine($"Nome: {name}");
+Console.WriteLine($"Idade: {age}");
+Console.WriteLine($"Cidade: {city}");
+Console.WriteLine($"Profissão dos Sonhos: {dreamprofission}");
